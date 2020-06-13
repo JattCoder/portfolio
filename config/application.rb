ad1ee35007dcb73de0bbbd54e33dc21c62ad1fc2
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-06-13 10:02:11 -0400

    api updated cors

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,19 @@ module Portfolio
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_portfolio_key'
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:3000'
+        resource(
+          '*',
+          headers: :any,
+          methods: [:get, :patch, :put, :delete, :post, :options]
+          )
+       end
+    end
+    config.api_only = true
+    config.api_only = true
   end
 end
