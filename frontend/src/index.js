3f99b6b45e26be92d9d43c34ace9065039a51e9d
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-06-13 12:26:47 -0400

    React - Redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk'
+import { Provider } from "react-redux";
+import reducer from './reducers'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
