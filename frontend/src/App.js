1babb4741ce9422e21e64cfeeb2861fe2597c1e8
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-06-13 10:35:43 -0400

    React - added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div style={{justifyContent: 'center',textAlign:'center'}}>
+          <Nav/>
+          <Switch>
+            <Route exact={true} path='/' component={Home}/>
+            <Route exact={true} path='/sorting' component={Sorting}/>
+            <Route exact={true} path='/apps' component={Apps}/>
+            <Route exact={true} path='/about' component={About}/>
+          </Switch>
+        </div>
+    </Router>
   );
 }
 
