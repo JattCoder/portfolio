20e7b7a8d2a67fe97ed38d18eb7ed5c4673d3451
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-06-25 22:12:20 -0400

    backend

--- frontend/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import quote from './getquote'
-import gethome from './gethome'
-
-const rootreducers =  combineReducers({
-  quote,gethome
-})
-
-export default rootreducers
